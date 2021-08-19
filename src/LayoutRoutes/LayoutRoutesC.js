6d9181290dba41a149b1eabbc744533ffe2547c8
Viet Anh Le <mc.vietanh@gmail.com>
2021-08-19 11:51:06 -0400

    updated

---
+++ src/LayoutRoutes/LayoutRoutesC.js
@@ -0,0 +1,108 @@
+import { lazy } from 'react';
+import { Switch, Redirect, Route } from 'react-router-dom';
+import { PrivateLayout, PublicLayout } from './Layouts';
+
+import HomePage from '../Pages/HomePage';
+
+function RouteWithSubRoutes(route) {
+  return (
+    <Route
+      path={route.path}
+      exact={route.exact}
+      render={(props) =>
+        route.layout ? (
+          <route.layout>
+            <route.component {...props} routes={route.routes} />
+          </route.layout>
+        ) : (
+          <route.component {...props} routes={route.routes} />
+        )
+      }
+    />
+  );
+}
+
+export function RenderRoutes({ routes }) {
+  return (
+    <Switch>
+      {routes.map((route, i) => {
+        return <RouteWithSubRoutes key={route.key} {...route} />;
+      })}
+      <Route component={() => <Redirect to='/404' />} />
+    </Switch>
+  );
+}
+const layoutRoutesConfig = [
+  {
+    exact: true,
+    path: '/',
+    component: () => <Redirect to='/home' />,
+  },
+  {
+    exact: true,
+    path: '/404',
+    component: lazy(() => import('../Pages/Page404')),
+  },
+  {
+    path: '/layoutroutes',
+    layout: PublicLayout,
+    component: RenderRoutes,
+    routes: [
+      {
+        exact: true,
+        path: '/layoutroutes/public',
+        component: () => <Redirect to='/layoutroutes/public/login' />,
+      },
+      {
+        exact: true,
+        path: '/layoutroutes/public/login',
+        component: lazy(() => import('../Pages/LoginPage')),
+      },
+      {
+        exact: true,
+        path: '/layoutroutes/public/register',
+        component: lazy(() => import('../Pages/AccountPage')),
+      },
+    ],
+  },
+  {
+    path: '/app',
+    layout: PrivateLayout,
+    component: RenderRoutes,
+    routes: [
+      {
+        exact: true,
+        path: '/app',
+        component: () => <Redirect to='/app/dashboard' />,
+      },
+      {
+        exact: true,
+        path: '/app/dashboard',
+        component: lazy(() => import('../Pages/Dashboard')),
+      },
+      {
+        exact: true,
+        path: '/app/account',
+        component: lazy(() => import('../Pages/AccountPage')),
+      },
+    ],
+  },
+  {
+    path: '*',
+    layout: PublicLayout,
+    component: RenderRoutes,
+    routes: [
+      {
+        exact: true,
+        path: '/home',
+        component: HomePage,
+      },
+    ],
+  },
+];
+
+function LayoutRoutesC() {
+  return <RenderRoutes routes={layoutRoutesConfig} />;
+}
+
+export default LayoutRoutesC;
