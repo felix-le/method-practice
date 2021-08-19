53f033b3761cefd1d33988cceac4caf484aab9cb
Viet Anh Le <mc.vietanh@gmail.com>
2021-08-19 11:45:00 -0400

    updated

--- src/LayoutRoutes/LayoutRoutesC.js
+++ src/LayoutRoutes/LayoutRoutesC.js
@@ -1,10 +1,8 @@
-import { lazy, Fragment, Suspense } from "react";
-import { Switch, Redirect, Route } from "react-router-dom";
-import { PrivateLayout, PublicLayout } from "./Layouts";
+import { lazy } from 'react';
+import { Switch, Redirect, Route } from 'react-router-dom';
+import { PrivateLayout, PublicLayout } from './Layouts';
 
-import LoadingPage from "../Pages/LoadingPage";
-
-import HomePage from "../Pages/HomePage";
+import HomePage from '../Pages/HomePage';
 
 function RouteWithSubRoutes(route) {
   return (
@@ -30,91 +28,78 @@ export function RenderRoutes({ routes }) {
       {routes.map((route, i) => {
         return <RouteWithSubRoutes key={route.key} {...route} />;
       })}
-      <Route path="/404" component={lazy(() => import("../Pages/Page404"))} />
-      <Redirect to="/404" />
+      <Route component={() => <Redirect to='/404' />} />
     </Switch>
   );
 }
 const layoutRoutesConfig = [
   {
     exact: true,
-    path: "/",
-    component: () => <Redirect to="/home" />,
+    path: '/',
+    component: () => <Redirect to='/home' />,
   },
   {
-    path: "/layoutroutes",
+    exact: true,
+    path: '/404',
+    component: lazy(() => import('../Pages/Page404')),
+  },
+  {
+    path: '/layoutroutes',
     layout: PublicLayout,
     component: RenderRoutes,
     routes: [
       {
         exact: true,
-        path: "/layoutroutes/public",
-        component: () => <Redirect to="/layoutroutes/public/login" />,
+        path: '/layoutroutes/public',
+        component: () => <Redirect to='/layoutroutes/public/login' />,
       },
       {
         exact: true,
-        path: "/layoutroutes/public/login",
-        component: lazy(() => import("../Pages/LoginPage")),
+        path: '/layoutroutes/public/login',
+        component: lazy(() => import('../Pages/LoginPage')),
       },
       {
         exact: true,
-        path: "/layoutroutes/public/register",
-        component: lazy(() => import("../Pages/AccountPage")),
+        path: '/layoutroutes/public/register',
+        component: lazy(() => import('../Pages/AccountPage')),
       },
     ],
   },
   {
-    path: "/app",
+    path: '/app',
     layout: PrivateLayout,
     component: RenderRoutes,
     routes: [
       {
         exact: true,
-        path: "/app",
-        component: () => <Redirect to="/app/dashboard" />,
+        path: '/app',
+        component: () => <Redirect to='/app/dashboard' />,
       },
       {
         exact: true,
-        path: "/app/dashboard",
-        component: lazy(() => import("../Pages/Dashboard")),
+        path: '/app/dashboard',
+        component: lazy(() => import('../Pages/Dashboard')),
       },
       {
         exact: true,
-        path: "/app/account",
-        component: lazy(() => import("../Pages/AccountPage")),
+        path: '/app/account',
+        component: lazy(() => import('../Pages/AccountPage')),
       },
     ],
   },
   {
-    path: "*",
+    path: '*',
     layout: PublicLayout,
     component: RenderRoutes,
     routes: [
       {
         exact: true,
-        path: "/home",
+        path: '/home',
         component: HomePage,
       },
     ],
   },
 ];
-
-// const renderRoutes = (routes) =>
-//   routes ? (
-//     <Suspense fallback={<LoadingPage />}>
-//       <Switch>
-//         {routes.map((route, index) => {
-//           const Guard = route.guard || Fragment;
-//           const Layout = route.layout || Fragment;
-//           const Component = route.component;
-
-//           return (
-//             {RouteWithSubRoutes(route)}
-//           );
-//         })}
-//       </Switch>
-//     </Suspense>
-//   ) : null;
 
 function LayoutRoutesC() {
   return <RenderRoutes routes={layoutRoutesConfig} />;
